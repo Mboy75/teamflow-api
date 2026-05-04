b137cfaf55dc397acdb5a4000741c3477309ead4
Mboy75 <allam.massi@.com>
2026-05-04 13:05:33 +0200

    Add environment-based database config

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,10 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 
 class Settings(BaseSettings):
+
+    ENV: str = "dev" # new variable to determine the environment
+
+
     PROJECT_NAME: str = "TeamFlow API"
     DEBUG: bool = True
 
@@ -9,12 +13,25 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     ALGORITHM: str = "HS256"
 
-    DATABASE_URL: str = Field(
-        default="sqlite:///./test.db"
-    )
+    DATABASE_URL: str | None = None
+    
+    
+    #str = Field(
+    #    default="sqlite:///./test.db"
+    #)
 
     class Config:
         env_file = ".env"
 
 
 settings = Settings()
+
+
+def get_database_url():
+    if settings.ENV == "test":
+        return "sqlite:///./test.db"
+
+    if settings.DATABASE_URL:
+        return settings.DATABASE_URL
+
+    raise ValueError("DATABASE_URL not set")
--- app/db/session.py
+++ app/db/session.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 from sqlalchemy import create_engine
+from app.core.config import settings, get_database_url
 
-DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = get_database_url()
 
 
 connect_args = {}
